56d635c1ab69186636c24fae7f2682c76b191fff
MostLikelyNotHuman <55810556+MostLikelyNotHuman@users.noreply.github.com>
2026-03-01 23:53:40 -0600

    initial commit, completed basic setup

---
+++ src/main/java/com/example/unit_2_final/Unit2FinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.unit_2_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit2FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit2FinalApplication.class, args);
+	}
+
+}
